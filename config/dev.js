affe891a67c75eb14bb58c6afb007d8f00806174
Brandon Fajardo <brandon@applydigital.co>
2018-05-19 14:07:23 -0700

    Playing with redis cache

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
   googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  cookieKey: '123123123'
+    "964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com",
+  googleClientSecret: "KnH-rZC23z4fr2CN4ISK4srN",
+  mongoURI: "mongodb://brandon:password@ds047478.mlab.com:47478/blog_dev",
+  cookieKey: "123123123"
 };
